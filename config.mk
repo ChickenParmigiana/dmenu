ffaa3ae69392842b301f71e7b915e6b461afb366
Roger Webb <garrettgiles222@gmail.com>
2021-03-21 05:22:24 -0400

    Update config.mk

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ VERSION = 5.0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = usr/X11R6/include
+X11LIB = usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
